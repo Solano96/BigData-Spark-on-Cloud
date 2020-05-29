f369509d995a4ca857f9c50700b3da18bd1b27b4
Solano96 <fransol0728@gmail.com>
2020-05-29 15:52:22 +0200

    Fix error in function call

--- filterDataset.py
+++ filterDataset.py
@@ -6,7 +6,7 @@ def filter_columns(sc, path_header, path_dataset, columns):
 
     # Get header names
     headers = sc.textFile(path_header).collect()
-    inputs = [head for head in headers if "@inputs" in head]
+    columns = [head for head in headers if "@inputs" in head]
     columns = map(str.strip, columns[0].replace('@inputs', '').split(','))
     columns.append('class')
 
