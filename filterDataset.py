8653d0460e485a8872b163c2e2555a661be48592
Solano96 <fransol0728@gmail.com>
2020-05-29 15:55:46 +0200

    Fix error in function parameters

--- filterDataset.py
+++ filterDataset.py
@@ -1,7 +1,7 @@
 from pyspark import SparkContext, SparkConf, SQLContext
 
 
-def filter_columns(sc, path_header, path_dataset, columns):
+def filter_columns(sc, path_header, path_dataset, columns_to_filter):
     """ Read data through path header and path data provided"""
 
     # Get header names
@@ -18,7 +18,7 @@ def filter_columns(sc, path_header, path_dataset, columns):
     for i in range(0, len(dataset.columns)):
         dataset = dataset.withColumnRenamed(dataset.columns[i], columns[i])
 
-    dataset = dataset.select(columns)
+    dataset = dataset.select(columns_to_filter)
     dataset.write.csv('./filteredC.small.training', header=True, mode="overwrite")
 
 
@@ -31,6 +31,6 @@ if __name__ == "__main__":
     path_dataset = '/user/datasets/ecbdl14/ECBDL14_IR2.data'
     columns = ['PSSM_central_1_Y', 'PSSM_r2_4_T', 'PSSM_r1_-3_T', 'PSSM_r1_2_T', 'PSSM_r1_0_I', 'PredSS_central', 'class']
 
-    dataset = filter_columns(sc, path_header, path_dataset)
+    dataset = filter_columns(sc, path_header, path_dataset, columns)
 
     sc.stop()
